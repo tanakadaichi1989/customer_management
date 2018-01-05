b22d2aebcd21fe9de72459070595c904dacffc97
田中大地 <tanakadaichi_1989@MacBook-Air.local>
2018-01-06 07:45:36 +0900

    Edit application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def full_page_name(title = "")
+
+    base_page = "Customer Management"
+
+    if title.empty?
+      base_page
+    else
+      title + " | " + base_page
+    end
+
+  end
 end
