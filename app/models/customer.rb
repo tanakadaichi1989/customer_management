af966818bdc3185bb04093810abace810bd28e30
田中大地 <tanakadaichi_1989@MacBook-Air.local>
2018-01-10 11:46:43 +0900

    Eit Regexp

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,8 @@
 class Customer < ApplicationRecord
+  validates :name,{presence:true}
+
+  validates :email,{presence:true,format:/\A[\w+\-]+@[\a-z\d-]+/}
+
+  validates :telephone,{presence:true,format:/\d{2,5}-\d{1,4}-\d{4}/}
+  validates :address,{presence:true}
 end
