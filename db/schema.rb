ce1a46fb76cb1cc72dab1a1da03307552f2f7717
田中大地 <tanakadaichi_1989@MacBook-Air.local>
2018-01-11 11:10:15 +0900

    Make contact_staff_id column to Contact model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110024733) do
+ActiveRecord::Schema.define(version: 20180111020940) do
 
   create_table "contacts", force: :cascade do |t|
     t.integer "staff_id"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180110024733) do
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "contact_staffid"
   end
 
   create_table "customers", force: :cascade do |t|
