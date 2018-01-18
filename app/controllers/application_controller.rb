e3b5c09d211c59c0c78762328cec8a9c897854c7
Daichi Tanaka <tanakadaichi_1989@MacBook-Air.local>
2018-01-18 16:53:02 +0900

    Make Contact Helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include ApplicationHelper
 
   def set_current_staff
     @current_staff = Staff.find_by(id: session[:user_id])
--- app/helpers/contact_helper.rb
+++ app/helpers/contact_helper.rb
@@ -1,2 +1,11 @@
 module ContactHelper
+  def staff_name(staff)
+    staff = staff
+    Staff.find_by(id: staff.staff_id).name
+  end
+
+  def company_name(company)
+    company = company
+    Customer.find_by(id: company.customer_id).company
+  end
 end
