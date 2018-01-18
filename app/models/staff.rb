fa6cedfffd59fc622749f8be7a3dcfca9e6d9d81
Daichi Tanaka <tanakadaichi_1989@MacBook-Air.local>
2018-01-18 13:42:15 +0900

    Edit password validation

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -6,7 +6,7 @@ class Staff < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,{presence: true, uniqueness: true, format:{with:/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i},length:{maximum:255}}
 
-  #validates :password,{presence: true, length:{minimum:8,maximum:128}}
+  validates :password,{presence: true, length:{minimum:8,maximum:128}}
 
   def contact
     return Contact.where(staff_id: self.id)
