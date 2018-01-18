34a339e44ba1a9b7522c5e83d70997e5c6cbe584
Daichi Tanaka <tanakadaichi_1989@MacBook-Air.local>
2018-01-18 13:30:47 +0900

    Edit Staff model Add has_secure_password

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,10 +1,12 @@
 class Staff < ApplicationRecord
+  has_secure_password
+
   validates :name,{presence: true}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,{presence: true, uniqueness: true, format:{with:/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i},length:{maximum:255}}
 
-  validates :password,{presence: true, length:{minimum:8,maximum:128}}
+  #validates :password,{presence: true, length:{minimum:8,maximum:128}}
 
   def contact
     return Contact.where(staff_id: self.id)
